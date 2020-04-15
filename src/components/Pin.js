0cf17adb237aa620b9eb3d4d864bcf153de388a1
TE-Good <tom.good1990@gmail.com>
2020-04-15 19:59:15 +0100

    adding pincode logic

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Pin() {
+  const [combination, setCombination] = useState([])
+
+  function handleCombination(event) {
+    const newCombination = [...combination, event.target.id]
+    setCombination(newCombination)
+    if (newCombination.length === 4) {
+      console.log('PINCODE', newCombination)
+      setCombination([])
+    }
+  }
+
   return (
     <div className="pin-grid">
-      <div className="pin-container"><div className="pin"></div></div>
-      <div className="pin-container"><div className="pin"></div></div>
-      <div className="pin-container"><div className="pin"></div></div>
-      <div className="pin-container"><div className="pin"></div></div>
-      <div className="pin-container"><div className="pin"></div></div>
-      <div className="pin-container"><div className="pin"></div></div>
-      <div className="pin-container"><div className="pin"></div></div>
-      <div className="pin-container"><div className="pin"></div></div>
-      <div className="pin-container"><div className="pin"></div></div>
-      <div className="pin-container"><div className="pin"></div></div>
-      <div className="pin-container"><div className="pin"></div></div>
-      <div className="pin-container"><div className="pin"></div></div>
+      {console.log('combination =', combination)}
+      <div className="pin-container"><div id="1" className="pin" onClick={handleCombination}></div></div>
+      <div className="pin-container"><div id="2" className="pin" onClick={handleCombination}></div></div>
+      <div className="pin-container"><div id="3" className="pin" onClick={handleCombination}></div></div>
+      <div className="pin-container"><div id="4" className="pin" onClick={handleCombination}></div></div>
+      <div className="pin-container"><div id="5" className="pin" onClick={handleCombination}></div></div>
+      <div className="pin-container"><div id="6" className="pin" onClick={handleCombination}></div></div>
+      <div className="pin-container"><div id="7" className="pin" onClick={handleCombination}></div></div>
+      <div className="pin-container"><div id="8" className="pin" onClick={handleCombination}></div></div>
+      <div className="pin-container"><div id="9" className="pin" onClick={handleCombination}></div></div>
+      <div className="pin-container"><div id="10" className="pin" onClick={handleCombination}></div></div>
+      <div className="pin-container"><div id="11" className="pin" onClick={handleCombination}></div></div>
+      <div className="pin-container"><div id="12" className="pin" onClick={handleCombination}></div></div>
     </div>
   )
 }
