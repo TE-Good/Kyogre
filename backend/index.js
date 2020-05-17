10eda095257440137b045bfa0156c5dc4cf878d8
TE-Good <tom.good1990@gmail.com>
2020-05-17 11:33:55 +0100

    added comments, and fixed mongoose connect

--- backend/index.js
+++ backend/index.js
@@ -18,15 +18,21 @@ const port = '8000'
 
 express.json()
 
-// mongoose.connect('http://localhost/test')
-
 const app = express()
 
+// Mongo connection and db connection log
+mongoose.connect('http://localhost/kyogre', { useNewUrlParser: true }, () => console.log('Mongo connected.'))
+
+
+// HTTP method & route call log
 app.use((req, res, next) => {
   console.log(`${req.method} to ${req.url}`)
-  next()
+  return next()
 })
 
+// Router & controllers
 app.get('/api/quote', (req, res) => res.send(quotes.quotes[moment().format('DDD') % quotes.quotes.length]))
 app.get('/api/random_quote', (req, res) => res.send(quotes.quotes[Math.floor(Math.random() * quotes.quotes.length)]))
+
+// Express connection log
 app.listen(port, () => console.log(`Receiving on port ${port}`))
