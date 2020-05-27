e4a63c3eff114802f474570e725cecdbd67c3470
TE-Good <tom.good1990@gmail.com>
2020-05-27 19:11:02 +0100

    added heroku vars

--- backend/enviro.js
+++ backend/enviro.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 
 // Enviroment settings
 const localDbURI = 'mongodb://localhost/kyogre'
-const dbURI = process.env.MONGODBATLASURI || localDbURI
+const dbURI = process.env.MONGODB_ATLAS_HEROKU_URI || process.env.MONGODB_ATLAS_URI || localDbURI
 
 module.exports = dbURI
