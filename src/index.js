9957b4a5f3ebe1ffd9a59fc1106b11f3e8da6a01
TE-Good <tom.good1990@gmail.com>
2020-06-18 19:55:12 +0100

    console message

--- src/index.js
+++ src/index.js
@@ -9,7 +9,34 @@ import Tenet from './components/Tenet'
 
 import './styles.scss'
 
+const kyogre = `
+                                                ':,.:xkc,oko
+                                            ';'.;:,',;;'';:,
+                                           .''......''''..  
+                              ....       ......'...''...    
+                              ..''...................       
+                    ..      ....''''''''''......            
+               ............'''''''''''''''''''..            
+            .....''...'...'''''';ccc:;,'''''''''..          
+        ..  ..'''''...''''..''',o0XNXKOl,''''''''.          
+    ................''''''..';:looodxxdc,'''''';c:.         
+  ....'........   ..,,;:,..'',;:c:,''''''',codkko'          
+      .....         ..';,''''..'cxxkkOOOkk0K0dc.            
+           ............''''.....;d0XXNX0xo:'.               
+      .....''''''''''..'''..      .';l:.                    
+  ,l;...'.....'''''''..'''.                                 
+ .:oc,'.'.........''''''..                                  
+  :O0d,'''...'''..''''..                                    
+  ,c:;lkOxl,,;c::;'...                                      
+     'x0Od;,oKXKx;.                                         
+      ...  'xK0o.                                                                                                                 
+`
+const note = `Thanks for taking a look at Kyogre Quotes!
+Made by https://github.com/TE-Good.`
+
 function App() {
+  console.log(kyogre)
+  console.log(note)
   return (
     <Router>
       <Switch>
